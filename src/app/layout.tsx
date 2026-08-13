99687f6c96aa49d843efa4168ebe5340c0cf889f
kiaacristiee <cristiehizkia@gmail.com>
2026-08-13 11:30:45 +0700

    logo changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,6 +48,8 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon.ico" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/icon.png", type: "image/png" },
     ],
     shortcut: ["/favicon.ico"],
